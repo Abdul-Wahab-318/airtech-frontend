49ebc43399117d7c1036d8ee5095152c81f692aa
Abdul-Wahab-318 <wahabmaliq@gmail.com>
2024-03-03 10:18:17 +0500

    increases tailwind specificity

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,10 @@
+import { Button } from '@mui/material'
 import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <Button variant="outlined" >click me</Button>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'black': '#080808',
+      'white': '#FFFFFF',
+      'yellow': '#ffbc14',
+      'yellow-light' : 'rgb(255, 242, 224)' ,
+      'text-contrast': '#947107',
+      'text': "#1B1B1B",
+      'text-light': '#5E5E5E'
+    }
   },
+  important : true ,
   plugins: [],
 }
